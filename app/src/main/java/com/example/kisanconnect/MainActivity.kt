b5c733c495975ca9e7aab04614e7e76ff72adf58
revanthkumarJ <jrevanth101@gmail.com>
2024-11-04 19:08:54 +0530

    Initial commit with navbar and drawer setUp

---
+++ app/src/main/java/com/example/kisanconnect/MainActivity.kt
@@ -0,0 +1,155 @@
+package com.example.kisanconnect
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.example.kisanconnect.ui.theme.KisanConnectTheme
+import kotlinx.coroutines.launch
+
+data class NavigationItem(
+    val title:String,
+    val selectedIcon:ImageVector,
+    val unselectedIcon:ImageVector,
+    val badgeCount:Int?=0
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KisanConnectTheme {
+
+                val items = listOf(
+                    NavigationItem("Home", Icons.Filled.Home, Icons.Outlined.Home),
+                    NavigationItem("About", Icons.Filled.Info, Icons.Outlined.Info),
+                    NavigationItem("Profile", Icons.Filled.Person, Icons.Outlined.Person, badgeCount = 10)
+                )
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val scope = rememberCoroutineScope()
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+                    var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
+
+                    ModalNavigationDrawer(
+                        drawerState = drawerState,
+                        drawerContent = {
+                            ModalDrawerSheet(modifier = Modifier.fillMaxWidth(0.9f)) {
+                                Spacer(modifier = Modifier.height(10.dp))
+                                items.forEachIndexed { index, item ->
+                                    NavigationDrawerItem(
+                                        label = { Text(text = item.title) },
+                                        selected = index == selectedItemIndex,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            scope.launch { drawerState.close() }
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = if (index == selectedItemIndex) item.selectedIcon else item.unselectedIcon,
+                                                contentDescription = item.title
+                                            )
+                                        },
+                                        badge = {
+                                            if (item.badgeCount!! > 0) {
+                                                Text(text = item.badgeCount.toString())
+                                            }
+                                        },
+                                        colors = NavigationDrawerItemDefaults.colors(
+                                            selectedContainerColor = Color(0xFFD8F3DC), // Light green for selected item
+                                            unselectedContainerColor = Color.Transparent
+                                        ),
+                                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                                    )
+                                }
+                            }
+                        }
+                    ) {
+                        Scaffold(
+                            modifier = Modifier.fillMaxSize(),
+                            topBar = {
+                                CenterAlignedTopAppBar(
+                                    title = { Text(text = "Kisan Connect") },
+                                    navigationIcon = {
+                                        IconButton(onClick = {
+                                            scope.launch { drawerState.open() }
+                                        }) {
+                                            Icon(
+                                                imageVector = Icons.Default.Menu,
+                                                contentDescription = "Menu"
+                                            )
+                                        }
+                                    },
+                                    scrollBehavior = scrollBehavior,
+                                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                                        containerColor = Color(0xFFA8E6CF), // Light green color for top bar
+                                        titleContentColor = Color.White,
+                                        navigationIconContentColor = Color.White,
+                                        actionIconContentColor = Color.White
+                                    )
+                                )
+                            }
+                        ) { innerPadding ->
+                            LazyColumn(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding)
+                            ) {
+                                items(100) {
+                                    Text(
+                                        text = it.toString(),
+                                        modifier = Modifier.padding(16.dp)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
